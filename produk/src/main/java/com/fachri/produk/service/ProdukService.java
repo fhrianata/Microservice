466a05a0609f2d0a886de50776b23e34096ee23a
FachriPutraAnata <fachrianata123@gmail.com>
2026-02-26 11:44:31 +0700

    Add files via upload

---
+++ produk/src/main/java/com/fachri/produk/service/ProdukService.java
@@ -0,0 +1,31 @@
+package com.fachri.produk.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fachri.produk.model.Produk;
+import com.fachri.produk.repository.ProdukRepository;
+
+@Service
+public class ProdukService {
+    @Autowired
+    private ProdukRepository produkRepository;
+
+    public List<Produk> getAllProduks(){
+        return produkRepository.findAll();
+    }
+
+    public Produk getProdukById(Long id){
+        return produkRepository.findById(id).orElse(null);
+    }
+
+    public Produk createProduk(Produk produk){
+        return produkRepository.save(produk);
+    }
+
+    public void deleteProduk(Long id){
+        produkRepository.deleteById(id);
+    }
+}
